75472b82d76310fa457e525a3d418c99ae567f9c
MestreDeNada <rbennet5@calstatela.edu>
2017-07-01 01:28:17 -0700

    Homework 1

--- app/src/main/java/edu/calstatela/cs594/rickbennett/newsapp/MainActivity.java
+++ app/src/main/java/edu/calstatela/cs594/rickbennett/newsapp/MainActivity.java
@@ -1,45 +1,87 @@
 package edu.calstatela.cs594.rickbennett.newsapp;
 
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    TextView mSearchResultsTextView;
+
+    ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_news_search_results_json);
+
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        makeNewsSearchQuery();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            mSearchResultsTextView.setText("");
+            makeNewsSearchQuery();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public class FetchNewsTask extends AsyncTask<String, void, String[]> {
+    public class FetchNewsTask extends AsyncTask<URL, Void, String> {
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
 
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String newsSearchResults = null;
+            try {
+                newsSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return newsSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (s != null && !s.equals("")) {
+                mSearchResultsTextView.setText(s);
+            }
+        }
+    }
+
+    private void makeNewsSearchQuery() {
+        URL newsSearchUrl = NetworkUtils.buildUrl();
+        new FetchNewsTask().execute(newsSearchUrl);
     }
 }
